d3560fc778f417c9dd17edab1ac1bd82b4bad3b3
Juan Emiliano Cicirello <juanemilianocicirello@gmail.com>
2020-06-02 19:01:32 -0300

    Ejercicio

--- Ejercicio23/EjecicioIFTS11Unidad3/Program.cs
+++ Ejercicio23/EjecicioIFTS11Unidad3/Program.cs
@@ -6,7 +6,7 @@ namespace EjecicioIFTS11Unidad3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello ");
         }
     }
 }
