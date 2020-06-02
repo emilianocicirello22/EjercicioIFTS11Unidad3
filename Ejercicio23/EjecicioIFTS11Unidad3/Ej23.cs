6b119d40c7188f8411a5bbbb2fdff1a0702462b4
Juan Emiliano Cicirello <juanemilianocicirello@gmail.com>
2020-06-02 19:14:03 -0300

    Ejercicio

---
+++ Ejercicio23/EjecicioIFTS11Unidad3/Ej23.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//23) Se necesita crear un programa que imprima lotes de 100 tiquets. La numeración de los tiquets tiene que tener 
+// saltos de 10 en diez.  Utilizando el ciclo de repetición Mientras – Hacer.
+
+namespace EjecicioIFTS11Unidad3
+{
+    class Ej23
+    {
+        static void Main(string[] args)
+        {
+            //declaramos variables
+            int cont = 0;
+
+            //imprimimos los ticket
+            while (cont < 1000)
+            {
+                //incrementamos el contador para que salte de 10 en 10
+                cont += 10;
+                //imprimimos el ticket
+                Console.WriteLine($"numero de ticket {cont}");
+            }
+        }
+    }
+}
