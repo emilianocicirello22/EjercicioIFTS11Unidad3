7cd24b5ffaf13da9a3e7602392074733dae63e5a
Juan Emiliano Cicirello <juanemilianocicirello@gmail.com>
2020-06-02 19:28:21 -0300

    Ejercicio

---
+++ Ejercicio23/Ejercicio25/Ej25.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejercicio25
+{
+    class Ej25
+    {
+        static void Main(string[] args)
+        {
+            //recorremos el for de 10 en 10
+            for(int i = 0; i <= 1000; i += 10)
+            {
+                //imprimimos el valor de ticket
+                Console.WriteLine($"Ticket numero {i}");
+            }
+        }
+    }
+}
