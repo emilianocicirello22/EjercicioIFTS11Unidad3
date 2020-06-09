6b4e0889be8cc4f0b17c19c646e7cc31a842a5f1
Juan Emiliano Cicirello <juanemilianocicirello@gmail.com>
2020-06-09 18:26:19 -0300

    Ejercicio

---
+++ Ejercicio23/Ejercicio27/Ej27.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//27. Informar las potencias de 2 a partir de 2^0   y como máximo 2^10 
+
+namespace Ejercicio27
+{
+    class Ej27
+    {
+        static void Main(string[] args)
+        {
+            //inicializamos variables
+            double potencia = 0;
+            
+            for(int i =0; i<=10; i++)
+            {
+                Console.WriteLine($"2^{i} = {Math.Pow(2, i)}");
+            }
+        }
+    }
+}
